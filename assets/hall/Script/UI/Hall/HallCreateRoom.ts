00d0b54aacf0130c120c39f59456c3f1d1a92f2a
dianbingx <764850250@qq.com>
2019-06-01 18:01:02 +0800

    fff

--- assets/hall/Script/UI/Hall/HallCreateRoom.ts
+++ assets/hall/Script/UI/Hall/HallCreateRoom.ts
@@ -16,7 +16,7 @@ const gameTables = {
 }
 
 @ccclass
-export default class NewClass extends UIComponent {
+export default class HallCreateRoom extends UIComponent {
 
     @property([cc.Node])
     gameTable: cc.Node;
@@ -28,9 +28,22 @@ export default class NewClass extends UIComponent {
     gameItem: cc.Node;
 
     @property([cc.SpriteFrame])
-    toggleSpriteFrame: cc.SpriteFrame[] = [];
+    backgroundSpriteFrame: cc.SpriteFrame[] = [];
+
+    @property([cc.SpriteFrame])
+    checkmarkSpriteFrame: cc.SpriteFrame[] = [];
 
     protected CacheGuidePath: string = "cacheGuide"
+
+    protected _gameNumList: cc.Node[] = [];
+    protected _gamePlayerList: cc.Node[] = [];
+    protected _gameOptionalList: cc.Node[] = [];
+    protected _gameWanfaList: cc.Node[] = [];
+    protected _gameZhuaniaoList: cc.Node[] = [];
+    protected _gameFengdingList: cc.Node[] = [];
+    protected _gameTuoguanList: cc.Node[] = [];
+    protected _gameLiankaiList: cc.Node[] = [];
+    protected _gameHupaiTipList: cc.Node[] = [];
 
     onEnable() {
         this.init();
@@ -39,18 +52,27 @@ export default class NewClass extends UIComponent {
     init() {
         let data = {
             '1': {
-                'gameNum': 'gameNumItem_|0|1:2',
-                'gamePlayer': 'gamePlayerItem_|0|18:16',
-                'gameOptional': 'gameOptionalItem_|0|5:20',
-                'gameWanfa': 'gameWanfaItem_|0|46:47:48:49:50:52:53:70:73:69:51',
-                'gameZhuaniao': 'gameZhuaniaoItem_|0|54:55:8:9:10',
-                'gameFengding': 'gameFengdingItem_|0|54',
-                'gameTuoguan': 'gameTuoguanItem_|0|1000:1001:1002',
-                'gameLiankai': 'gameLiankaiItem_|0|3000:3001:3002:3003',
-                'gameHupaiTip': 'gameHupaiTipItem_|0|2000',
+                'gameNum': 'gameNumItem_|0|0|1:2',
+                'gamePlayer': 'gamePlayerItem_|1|0|18:16',
+                'gameOptional': 'gameOptionalItem_|0|0|5:20',
+                'gameWanfa': 'gameWanfaItem_|1|0|46:47:48:49:50:52:53:70:73:69:51',
+                'gameZhuaniao': 'gameZhuaniaoItem_|0|0|54:55:8:9:10',
+                'gameFengding': 'gameFengdingItem_|1|0|54',
+                'gameTuoguan': 'gameTuoguanItem_|0|0|1000:1001:1002',
+                'gameLiankai': 'gameLiankaiItem_|0|0|3000:3001:3002:3003',
+                'gameHupaiTip': 'gameHupaiTipItem_|0|0|2000',
             }
         }
         this.instantiateTable(data);
+        let Historydata = {
+            'playTypes': [1,1,3,10],
+            'huTypes': [2,3,4],
+            'numOfGame': 1,
+            'roomSource': 0,
+            'clubId': 0,
+            'gameType': 1,
+            'gameModel': 1
+        }
     }
 
     //data  
@@ -62,17 +84,28 @@ export default class NewClass extends UIComponent {
                 gameNum.name = gameTables[n];
                 this.gameTable.addChild(gameNum);
                 let tb = data[gameTables[n]].split('|');
-                let tbWanfa = tb[2].split(':');
+                let b_toggleSprite = gameNum.getComponent(cc.ToggleContainer);
+                if(tb[1] == 0){
+                    b_toggleSprite.allowSwitchOff = false;
+                }else{
+                    b_toggleSprite.allowSwitchOff = true;
+                }
+                let tbWanfa = tb[3].split(':');
                 for (let i = 0; i < tbWanfa.length; i++) {
                     let gameItem = cc.instantiate(this.gameItem);
                     gameItem.name = tb[0] + tbWanfa[i];
                     gameNum.addChild(gameItem);
                     let toggle = gameItem.getChildByName('toggle');
-                    let toggleSprite = toggle.getComponent(cc.Sprite);
-                    if(tb[1] == 0){
-                        toggleSprite.spriteFrame = this.toggleSpriteFrame[0];
+                    let Background = toggle.getChildByName('Background');
+                    let b_toggleSprite = Background.getComponent(cc.Sprite);
+                    let checkmark = toggle.getChildByName('checkmark');
+                    let c_toggleSprite = checkmark.getComponent(cc.Sprite);
+                    if(tb[2] == 0){
+                        b_toggleSprite.spriteFrame = this.backgroundSpriteFrame[0];
+                        c_toggleSprite.spriteFrame = this.checkmarkSpriteFrame[0];
                     }else{
-                        toggleSprite.spriteFrame = this.toggleSpriteFrame[1];
+                        b_toggleSprite.spriteFrame = this.backgroundSpriteFrame[1];
+                        c_toggleSprite.spriteFrame = this.checkmarkSpriteFrame[1];
                     }
                     let wanfaLabel = gameItem.getChildByName('wanfaLabel');
                     let Label = wanfaLabel.getComponent(cc.Label);
@@ -80,5 +113,45 @@ export default class NewClass extends UIComponent {
                 }
             }
         }
+    }
+
+    _onBtnCreateRoomTouchStart() {
+        let data = {
+            'playTypes': [],
+            'huTypes': [],
+            'numOfGame': 0,
+            'roomSource': 0,
+            'clubId': 0,
+            'gameType': 0,
+            'gameModel': 0
+        }
+        for (let n = 0; n < 9; n++){
+            let gameNumContent = this.gameTable.getChildByName(gameTables[n]);
+            if(gameNumContent){
+                for(let i = 0;i<gameNumContent.childrenCount;i++){
+                    let item = gameNumContent.children[i];
+                    let itemName = item.name;
+                    let _Num=itemName.indexOf("_");
+                    let itemID = itemName.substr(_Num);
+                    let toggle = item.getChildByName('toggle');
+                    let toggle_script = toggle.getComponent(cc.Toggle);
+                    if(toggle_script.isChecked){
+                        data = this.setupCreateRoomData(gameTables[n],itemID,data);
+                    }
+                }
+            }
+        }
+        console.log(data);
+    }
+
+    setupCreateRoomData(type,id,data){
+        
+        if(type == gameTables[0]){
+            data.playTypes.push(id);
+        }
+        if(type == gameTables[2]){
+            data.numOfGame = id;
+        }
+        return data;
     }
 }
